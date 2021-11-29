a265d4cb6ac088f575efa33144e150ddf005ed0d
MdHasanRaju <mdhasanraju2017@gmail.com>
2021-11-29 22:35:01 +0500

    contact form worked

--- src/components/Mailer/Mailer.js
+++ src/components/Mailer/Mailer.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import emailjs from 'emailjs-com';
 
 const Mailer = () => {
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('service_kiqwmpa', 'template_lwo935u', e.target, 'user_rQcSrqu2UAQtEHCLMDNzr')
+        .then(res => {
+            console.log(res);
+        }).catch(err => console.log(err));
+    }
+
     return (
         <div className="container border" style={{marginTop:"50px", width:"50%", backgroundImage:`url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuw_C9TmXXBy-9q5UZdv3jxslpK9qn_bBmiA&usqp=CAU')`, backgroundPosition:"center", backgroundSize:"cover"}}>
-            <h2 style={{marginTop:"30px"}}>Contact Form</h2>
-            <form className="row" style={{margin:"25px 85px 75px 100px"}}>
-                <label>Name</label>
+            <h2 style={{marginTop:"30px", textAlign:"center"}}>Contact Form</h2>
+            <form className="row" style={{margin:"25px 85px 75px 100px"}} onSubmit={sendEmail}>
+                <label>name</label>
                 <input type="text" name="name" id="" className="form-control"/>
                 <label>Email</label>
                 <input type="email" name="user_email" id="" className="form-control" />
