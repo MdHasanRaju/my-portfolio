b95b8ca897e5f545be0f7506df876033c909557a
MdHasanRaju <mdhasanraju2017@gmail.com>
2021-11-30 20:52:03 +0500

    colored

--- src/Navigation.js
+++ src/Navigation.js
@@ -11,13 +11,13 @@ const Navigation = () => {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ms-auto">
-            <Link style={{margin:"auto 5px", textDecoration:"none", color:"lightgray"}} to="/home">Home</Link>
+            <Link className="tggle" style={{margin:"auto 5px", textDecoration:"none", color:"lightgray"}} to="/home">Home</Link>
 
-            <Nav.Link style={{margin:"auto 5px", textDecoration:"none", color:"lightgray"}} href="/home/#about">About</Nav.Link>
+            <Nav.Link className="tggle" style={{margin:"auto 5px", textDecoration:"none", color:"lightgray"}} href="/home/#about">About</Nav.Link>
 
-            <Link style={{margin:"auto 5px", textDecoration:"none", color:"lightgray"}} to="/blogs">Blogs</Link>
-              <Link style={{margin:"auto 5px", textDecoration:"none", color:"lightgray"}} to="/contact">Contact Me</Link>
-              <Nav.Link style={{margin:"auto 0" ,color:"lightgray"}} target="_blank" href="https://drive.google.com/file/d/1sSgJ96jnMrLnjFBA3m5_LYako40PYpJC/view?fbclid=IwAR0cMBbmax5fMvklZgjl3jBS-7eBpr2U2bGCRkCky4ltqNCz-2fRJ3EkW2A" download>Resume</Nav.Link>
+            <Link className="tggle" style={{margin:"auto 5px", textDecoration:"none", color:"lightgray"}} to="/blogs">Blogs</Link>
+              <Link className="tggle" style={{margin:"auto 5px", textDecoration:"none", color:"lightgray"}} to="/contact">Contact Me</Link>
+              <Nav.Link className="tggle" style={{margin:"auto 0" ,color:"lightgray"}} target="_blank" href="https://drive.google.com/file/d/1sSgJ96jnMrLnjFBA3m5_LYako40PYpJC/view?fbclid=IwAR0cMBbmax5fMvklZgjl3jBS-7eBpr2U2bGCRkCky4ltqNCz-2fRJ3EkW2A" download>Resume</Nav.Link>
               <Nav.Link >
               <Button style={{borderRadius:"40px", padding:"10px 30px"}} className="clr-code" >Case Studies</Button>{' '}
               </Nav.Link>
