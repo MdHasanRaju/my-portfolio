1b176a2e09da434876b5ac742f05452d0eaabd53
MdHasanRaju <mdhasanraju2017@gmail.com>
2021-11-30 22:04:54 +0500

    text-colored

--- src/components/Home/Footer/Footer.js
+++ src/components/Home/Footer/Footer.js
@@ -5,9 +5,9 @@ const Footer = () => {
   return (
     <div style={{backgroundColor:"black", color:"white", paddingTop:"30px", marginTop:"30px"}}>
       <div style={{ textAlign: "center"}}>
-        <i className="fab fa-facebook me-4 fs-3"></i>
-        <i className="fab fa-linkedin me-4 fs-3"></i>
-        <i className="fab fa-github me-4 fs-3"></i>
+        <a className="tggle" style={{ color:"white"}} target="_blank" href="https://www.facebook.com/hasanmahmudraju.raju"><i className="fab fa-facebook me-4 fs-3"></i></a>
+        <a className="tggle" style={{ color:"white"}} target="_blank" href="https://www.linkedin.com/in/md-mahmudul-hasan-1a8145227/"><i className="fab fa-linkedin me-4 fs-3"></i></a>
+        <a className="tggle" style={{ color:"white"}} target="_blank" href="https://github.com/MdHasanRaju"><i className="fab fa-github me-4 fs-3"></i></a>
       </div>
 
       <div style={{textAlign:"center", marginTop:"10px", paddingBottom:"4px"}}>
