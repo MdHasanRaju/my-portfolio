a59e167985cc0a8d4888250d960fe17fbf4d0c67
MdHasanRaju <mdhasanraju2017@gmail.com>
2022-01-23 17:04:25 +0600

    navbar fixed edited

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div style={{height:"100vh"}} >
+        <div style={{height:"100vh", marginTop:"100px"}} >
             <h4 className="text-center mt-5">Don't hurry please, they will come soon. I am writing blogs. </h4>
         </div>
     );
--- src/components/Mailer/Mailer.js
+++ src/components/Mailer/Mailer.js
@@ -16,7 +16,7 @@ const Mailer = () => {
     }
 
     return (
-        <div className="container border" style={{marginTop:"50px", padding:"20px 10px", minWidth:"350px", maxWidth:"50%", backgroundImage:`url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuw_C9TmXXBy-9q5UZdv3jxslpK9qn_bBmiA&usqp=CAU')`, backgroundPosition:"center", backgroundSize:"cover"}}>
+        <div className="container border" style={{marginTop:"100px", padding:"20px 10px", minWidth:"350px", maxWidth:"50%", backgroundImage:`url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuw_C9TmXXBy-9q5UZdv3jxslpK9qn_bBmiA&usqp=CAU')`, backgroundPosition:"center", backgroundSize:"cover"}}>
             <h2 style={{marginTop:"30px", textAlign:"center"}}>Contact Form</h2>
             <form className="row" style={{margin:"0 auto"}} onSubmit={sendEmail}>
                 <label>Name</label>
